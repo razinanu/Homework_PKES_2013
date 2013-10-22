003e60e769cb60ad6acf28407c822d6a63fad1d8
RaziNanu <razi.ghassemi@gmail.com>
2013-10-22 12:30:47 +0200

    first Assignment

---
+++ PKES1/def.h
@@ -0,0 +1,15 @@
+/*
+ * def.h
+ *
+ *  Created on: Sep 20, 2013
+ *      Author: zug
+ */
+
+#ifndef DEF_H_
+#define DEF_H_
+
+#define COUNTER_CYCLE 1
+#define MAX_COUNTER_VALUE 126
+#define MIN_COUNTER_VALUE -127
+
+#endif /* DEF_H_ */
